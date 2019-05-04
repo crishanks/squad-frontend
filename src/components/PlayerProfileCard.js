4f3dd55050b21fa823cd353f1426eaa7387d7ffb
github username <crishanks12@gmail.com>
2019-05-03 17:39:24 -0700

    welcome banner styling

--- src/components/PlayerProfileCard.js
+++ src/components/PlayerProfileCard.js
@@ -15,16 +15,19 @@ const PlayerProfileCard = (props) => {
       </div>
       <div id="name-box">
         <div className="header">{props.player.name}, {props.player.age}</div>
-        <div> <span className="bold"> Position:</span> {props.player.position}</div>
-        <div> <span className="bold"> Height:</span>  {props.player.height}</div>
-        <div><span className="bold"> Skill Level:</span>  {props.player.experience_level}</div>
+        <div> <span className="bold">Position:</span> {props.player.position}</div>
+        <div> <span className="bold">Height:</span>  {props.player.height}</div>
+        <div> <span className="bold">Skill Level:</span>  {props.player.experience_level}</div>
       </div>
       <div id="description-box">
         <div>{props.player.description}</div>
       </div>
       <div id="profile-card-footer">
-        <div className="circle" id="decline-circle" onClick={() => props.declinePlayer(props.player)}></div>
+        <div className="circle" id="decline-circle" onClick={() => props.declinePlayer(props.player)}>
+          <img src="../src/assets/images/icons/x.png" alt="x"/>
+        </div>
         <div className="circle" id="accept-circle" onClick={(props) => handleChoosePlayer(props)}>
+          <img src="../src/assets/images/icons/check.png" alt="check"/>
         </div>
       </div>
     </div>
--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -3,11 +3,12 @@ import React from 'react'
 
 const WelcomeBanner = (props) => {
   return (
+    <div id="welcome-banner-container">
       <div className='hero-content'>
-        <h1>Squad</h1>
-        <p>Squad</p>
-        <button onClick={props.signupClick}>Sign Up</button>
-        <button onClick={props.showLoginFormClick}>Log In</button>
+          <h1>SQUAD</h1>
+          <button onClick={props.signupClick}>| Sign Up |</button>
+          <button onClick={props.showLoginFormClick}>| Log In |</button>
+      </div>
     </div>
   )
 }
