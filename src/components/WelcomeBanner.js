20c056eb6f6358249bd8cf50fbc46395a85ff4dd
github username <crishanks12@gmail.com>
2019-05-08 10:14:13 -0700

    welcome screen layout better for smaller screens. added grid for new layout

--- src/components/WelcomeBanner.js
+++ src/components/WelcomeBanner.js
@@ -4,13 +4,9 @@ import React from 'react'
 const WelcomeBanner = (props) => {
   return (
     <div id="welcome-banner-container">
-      <div className='hero-content'>
-          <h1 className="logo">SQUAD</h1>
-          <div id="welcome-inputs">
-            <div onClick={props.signupClick}>Sign Up</div><br/>
-            <div onClick={props.showLoginFormClick}>Log In</div>
-          </div>
-      </div>
+      <h1 id="welcome-logo">SQUAD</h1>
+      <div id="welcome-signup" onClick={props.signupClick}>Sign Up</div><br/>
+      <div id="welcome-login" onClick={props.showLoginFormClick}>Log In</div>
     </div>
   )
 }
